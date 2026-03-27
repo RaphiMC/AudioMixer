b96eed4a91f434a78ce9bb79653f0d579ffee56f
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2026-03-27 23:42:58 +0100

    Added MixSound#removeSoundIf

--- src/main/java/net/raphimc/audiomixer/sound/impl/mix/MixSound.java
+++ src/main/java/net/raphimc/audiomixer/sound/impl/mix/MixSound.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class MixSound extends Sound {
 
@@ -84,6 +85,10 @@ public class MixSound extends Sound {
 
     public synchronized void forEachSound(final Consumer<Sound> action) {
         this.sounds.forEach(action);
+    }
+
+    public synchronized void removeSoundIf(final Predicate<Sound> predicate) {
+        this.sounds.removeIf(predicate);
     }
 
     public int getMaxSounds() {
