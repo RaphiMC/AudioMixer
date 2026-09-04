3a47ce9d6dbf21ec63dcc97f6e0f547ab13b6013
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2026-09-04 23:43:28 +0200

    Added windowed sinc resampler

---
+++ src/main/java/net/raphimc/audiomixer/resampler/impl/SincResampler.java
@@ -0,0 +1,176 @@
+/*
+ * This file is part of AudioMixer - https://github.com/RaphiMC/AudioMixer
+ * Copyright (C) 2024-2026 RK_01/RaphiMC and contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.raphimc.audiomixer.resampler.impl;
+
+import net.raphimc.audiomixer.resampler.Resampler;
+import net.raphimc.audiomixer.util.math.MathUtil;
+
+import java.util.Arrays;
+
+public abstract class SincResampler extends Resampler {
+
+    private final int tapCount;
+    private final int phaseCount;
+    private final double rolloff;
+    private final float[][] coefficientsTable;
+    private double cutoff;
+
+    public SincResampler() {
+        this(64);
+    }
+
+    public SincResampler(final int tapCount) {
+        this(tapCount, 128);
+    }
+
+    public SincResampler(final int tapCount, final int phaseCount) {
+        this(tapCount, phaseCount, 0.95D);
+    }
+
+    public SincResampler(final int tapCount, final int phaseCount, final double rolloff) {
+        super(tapCount / 2 - 1, tapCount / 2);
+        if (tapCount % 2 != 0 || tapCount < 2) {
+            throw new IllegalArgumentException("Tap count must be even and >= 2");
+        }
+        if (phaseCount <= 0) {
+            throw new IllegalArgumentException("Phase count must be > 0");
+        }
+        if (rolloff <= 0D || rolloff > 1D) {
+            throw new IllegalArgumentException("Rolloff must be in (0, 1]");
+        }
+        this.tapCount = tapCount;
+        this.phaseCount = phaseCount;
+        this.rolloff = rolloff;
+        this.coefficientsTable = new float[this.phaseCount + 1][];
+    }
+
+    @Override
+    protected void resampleMonoToMono(final float[] src, final float[] dst, final int outputFrameCount, final double srcStep, final double baseSrcFramePosition) {
+        this.updateCutoff(srcStep);
+        for (int dstFrameIndex = 0; dstFrameIndex < outputFrameCount; dstFrameIndex++) {
+            dst[dstFrameIndex] = this.sample(src, MathUtil.multiplyAndAdd(dstFrameIndex, srcStep, baseSrcFramePosition), 1, 0);
+        }
+    }
+
+    @Override
+    protected void resampleStereoToStereo(final float[] src, final float[] dst, final int outputFrameCount, final double srcStep, final double baseSrcFramePosition) {
+        this.updateCutoff(srcStep);
+        for (int dstFrameIndex = 0; dstFrameIndex < outputFrameCount; dstFrameIndex++) {
+            final double srcFramePosition = MathUtil.multiplyAndAdd(dstFrameIndex, srcStep, baseSrcFramePosition);
+            dst[dstFrameIndex * 2] = this.sample(src, srcFramePosition, 2, 0);
+            dst[dstFrameIndex * 2 + 1] = this.sample(src, srcFramePosition, 2, 1);
+        }
+    }
+
+    @Override
+    protected void resampleMonoToStereo(final float[] src, final float[] dst, final int outputFrameCount, final double srcStep, final double baseSrcFramePosition) {
+        this.updateCutoff(srcStep);
+        for (int dstFrameIndex = 0; dstFrameIndex < outputFrameCount; dstFrameIndex++) {
+            final float sample = this.sample(src, MathUtil.multiplyAndAdd(dstFrameIndex, srcStep, baseSrcFramePosition), 1, 0);
+            dst[dstFrameIndex * 2] = sample;
+            dst[dstFrameIndex * 2 + 1] = sample;
+        }
+    }
+
+    @Override
+    protected void resampleStereoToMono(final float[] src, final float[] dst, final int outputFrameCount, final double srcStep, final double baseSrcFramePosition) {
+        this.updateCutoff(srcStep);
+        for (int dstFrameIndex = 0; dstFrameIndex < outputFrameCount; dstFrameIndex++) {
+            final int srcFrameCount = src.length / 2;
+            final double srcFramePosition = MathUtil.multiplyAndAdd(dstFrameIndex, srcStep, baseSrcFramePosition);
+            final int srcFrameIndex = (int) srcFramePosition;
+            final int firstFrameIndex = srcFrameIndex - this.getLookBehindFrameCount();
+            final double phasePosition = (srcFramePosition - srcFrameIndex) * this.phaseCount;
+            final int phaseIndex = (int) phasePosition;
+            final float[] lowerPhaseCoefficients = this.getPhaseCoefficients(phaseIndex);
+            final float[] upperPhaseCoefficients = this.getPhaseCoefficients(phaseIndex + 1);
+            float sample = 0F;
+            for (int tapIndex = 0; tapIndex < lowerPhaseCoefficients.length; tapIndex++) {
+                final int tapSrcFrameIndex = firstFrameIndex + tapIndex;
+                if (tapSrcFrameIndex >= 0 && tapSrcFrameIndex < srcFrameCount) {
+                    final float coefficient = MathUtil.lerp(lowerPhaseCoefficients[tapIndex], upperPhaseCoefficients[tapIndex], (float) (phasePosition - phaseIndex));
+                    sample = MathUtil.multiplyAndAdd((src[tapSrcFrameIndex * 2] + src[tapSrcFrameIndex * 2 + 1]) / 2F, coefficient, sample);
+                }
+            }
+            dst[dstFrameIndex] = sample;
+        }
+    }
+
+    private void updateCutoff(final double srcStep) {
+        final double cutoff = this.rolloff / Math.max(1D, srcStep);
+        if (this.cutoff != cutoff) {
+            this.cutoff = cutoff;
+            Arrays.fill(this.coefficientsTable, null);
+        }
+    }
+
+    private float[] getPhaseCoefficients(final int phaseIndex) {
+        if (this.coefficientsTable[phaseIndex] == null) {
+            final double phase = (double) phaseIndex / this.phaseCount;
+            final float[] phaseCoefficients = new float[this.tapCount];
+            float coefficientsSum = 0F;
+            for (int tapIndex = 0; tapIndex < phaseCoefficients.length; tapIndex++) {
+                final double distance = tapIndex - this.getLookBehindFrameCount() - phase;
+                final float coefficient = (float) (this.cutoff * sinc(this.cutoff * distance) * this.evaluateWindow(distance / this.getLookAheadFrameCount()));
+                phaseCoefficients[tapIndex] = coefficient;
+                coefficientsSum += coefficient;
+            }
+            for (int tapIndex = 0; tapIndex < phaseCoefficients.length; tapIndex++) {
+                phaseCoefficients[tapIndex] /= coefficientsSum;
+            }
+            this.coefficientsTable[phaseIndex] = phaseCoefficients;
+        }
+        return this.coefficientsTable[phaseIndex];
+    }
+
+    protected abstract double evaluateWindow(final double normalizedDistance);
+
+    private float sample(final float[] samples, final double framePosition, final int channelCount, final int channelIndex) {
+        final int frameIndex = (int) framePosition;
+        final double phasePosition = (framePosition - frameIndex) * this.phaseCount;
+        final int phaseIndex = (int) phasePosition;
+        return convolve(
+            samples, frameIndex - this.getLookBehindFrameCount(),
+            channelCount, channelIndex,
+            this.getPhaseCoefficients(phaseIndex), this.getPhaseCoefficients(phaseIndex + 1),
+            (float) (phasePosition - phaseIndex)
+        );
+    }
+
+    private static float convolve(final float[] samples, final int firstFrameIndex, final int channelCount, final int channelIndex, final float[] lowerPhaseCoefficients, final float[] upperPhaseCoefficients, final float phaseFraction) {
+        float sample = 0F;
+        final int frameCount = samples.length / channelCount;
+        for (int tapIndex = 0; tapIndex < lowerPhaseCoefficients.length; tapIndex++) {
+            final int tapFrameIndex = firstFrameIndex + tapIndex;
+            if (tapFrameIndex >= 0 && tapFrameIndex < frameCount) {
+                final float coefficient = MathUtil.lerp(lowerPhaseCoefficients[tapIndex], upperPhaseCoefficients[tapIndex], phaseFraction);
+                sample = MathUtil.multiplyAndAdd(samples[tapFrameIndex * channelCount + channelIndex], coefficient, sample);
+            }
+        }
+        return sample;
+    }
+
+    private static double sinc(final double x) {
+        if (x == 0D) {
+            return 1D;
+        }
+        final double pix = Math.PI * x;
+        return MathUtil.sin(pix) / pix;
+    }
+
+}
--- src/main/java/net/raphimc/audiomixer/util/math/MathUtil.java
+++ src/main/java/net/raphimc/audiomixer/util/math/MathUtil.java
@@ -29,20 +29,30 @@ public final class MathUtil {
 
     public static final double HALF_PI = Math.PI / 2D;
     public static final double TWO_PI = Math.PI * 2D;
+    public static final double INV_PI = 1D / Math.PI;
     public static final double INV_SQRT_2 = 1D / Math.sqrt(2D);
     public static final float BUTTERWORTH_Q = (float) INV_SQRT_2;
 
-    private static final boolean SUPPORTS_FMA;
+    private static final double SIN_C1 = Double.longBitsToDouble(-4628199217061079959L);
+    private static final double SIN_C2 = Double.longBitsToDouble(4575957461383549981L);
+    private static final double SIN_C3 = Double.longBitsToDouble(-4671919876307284301L);
+    private static final double SIN_C4 = Double.longBitsToDouble(4523617213632129738L);
+    private static final double SIN_C5 = Double.longBitsToDouble(-4730215344060517252L);
+    private static final double SIN_C6 = Double.longBitsToDouble(4460268259291226124L);
+    private static final double SIN_C7 = Double.longBitsToDouble(-4798040743777455072L);
+    private static final boolean USE_FMA;
+    private static final boolean USE_FAST_SIN;
 
     static {
         boolean supportsFma = false;
-        try {
-            if (!Boolean.parseBoolean(System.getProperty("audioMixer.disableFma", "false"))) {
+        if (!Boolean.parseBoolean(System.getProperty("audioMixer.disableFma", "false"))) {
+            try {
                 supportsFma = Boolean.parseBoolean(ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class).getVMOption("UseFMA").getValue());
+            } catch (final Throwable ignored) {
             }
-        } catch (final Throwable ignored) {
         }
-        SUPPORTS_FMA = supportsFma;
+        USE_FMA = supportsFma;
+        USE_FAST_SIN = !Boolean.parseBoolean(System.getProperty("audioMixer.disableFastSin", "false"));
     }
 
     private MathUtil() {
@@ -84,28 +94,20 @@ public final class MathUtil {
         return multiplyAndAdd(b - a, t, a);
     }
 
-    public static float multiplyAndAdd(final float a, final float b, final float c) {
-        if (SUPPORTS_FMA) {
-            return Math.fma(a, b, c);
-        } else {
-            return a * b + c;
-        }
-    }
-
-    public static double multiplyAndAdd(final double a, final double b, final double c) {
-        if (SUPPORTS_FMA) {
-            return Math.fma(a, b, c);
-        } else {
-            return a * b + c;
-        }
+    public static double lerp(final double a, final double b, final double t) {
+        return multiplyAndAdd(b - a, t, a);
     }
 
     public static int roundDownToMultiple(final int value, final int multiple) {
-        return (value / multiple) * multiple;
+        return Math.multiplyExact(Math.floorDiv(value, multiple), multiple);
     }
 
     public static int roundUpToMultiple(final int value, final int multiple) {
-        return Math.multiplyExact(ceil((double) value / multiple), multiple);
+        if (Math.floorMod(value, multiple) != 0) {
+            return Math.multiplyExact(Math.floorDiv(value, multiple) + 1, multiple);
+        } else {
+            return value;
+        }
     }
 
     public static float dbToGain(final float db) {
@@ -114,6 +116,62 @@ public final class MathUtil {
 
     public static float gainToDb(final float gain) {
         return (float) (20D * Math.log10(gain));
+    }
+
+    public static double sin(final double v) {
+        if (USE_FAST_SIN) {
+            return fastSin(v);
+        } else {
+            return Math.sin(v);
+        }
+    }
+
+    public static double cos(final double v) {
+        if (USE_FAST_SIN) {
+            return fastCos(v);
+        } else {
+            return Math.cos(v);
+        }
+    }
+
+    public static float multiplyAndAdd(final float a, final float b, final float c) {
+        if (USE_FMA) {
+            return Math.fma(a, b, c);
+        } else {
+            return a * b + c;
+        }
+    }
+
+    public static double multiplyAndAdd(final double a, final double b, final double c) {
+        if (USE_FMA) {
+            return Math.fma(a, b, c);
+        } else {
+            return a * b + c;
+        }
+    }
+
+    /**
+     * Copyright <a href="https://github.com/JOML-CI/JOML/blob/c8f2ec39d9f138f9708bc7ac27a23e9603f14751/src/main/java/org/joml/Math.java#L176">JOML</a>.<br>
+     * Licensed under the <a href="https://github.com/JOML-CI/JOML/blob/c8f2ec39d9f138f9708bc7ac27a23e9603f14751/LICENSE">MIT</a> license.
+     */
+    public static double fastSin(final double v) {
+        final double i = Math.rint(v * INV_PI);
+        final double x0 = multiplyAndAdd(-i, Math.PI, v);
+        final double sign = 1D - 2D * ((int) i & 1);
+        final double x = sign * x0;
+        final double x2 = x * x;
+        double r = SIN_C7;
+        r = multiplyAndAdd(r, x2, SIN_C6);
+        r = multiplyAndAdd(r, x2, SIN_C5);
+        r = multiplyAndAdd(r, x2, SIN_C4);
+        r = multiplyAndAdd(r, x2, SIN_C3);
+        r = multiplyAndAdd(r, x2, SIN_C2);
+        r = multiplyAndAdd(r, x2, SIN_C1);
+        return multiplyAndAdd(x * x2, r, x);
+    }
+
+    public static double fastCos(final double v) {
+        return fastSin(v + HALF_PI);
     }
 
 }
